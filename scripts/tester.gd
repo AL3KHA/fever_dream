cedb7de0bfd5f709276b73e353ca3109fa00ba77
K0PALA <kopalianialeksandre1@gmail.com>
2026-09-20 01:06:19 +0400

    added realistic camera focus
    need to tweak focus speed

--- scripts/tester.gd
+++ scripts/tester.gd
@@ -1,5 +1,13 @@
 extends Node
 
+@onready var player = $player
+@onready var advanced_DOF = $player/Camera3D/SpringArm3D
+@onready var world_enviroment = $WorldEnvironment
+
+var far_distance_smoothed: float
+var near_distance_smoothed: float
+
+@export var DOF_speed: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +19,19 @@ func _input(event: InputEvent) -> void:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		elif Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	var dof_distance: float = advanced_DOF.get_hit_length()
+	var far_distance: float
+	var near_distance: float = dof_distance
+	if dof_distance <= 100:
+		far_distance = dof_distance
+	if dof_distance >= 95:
+		far_distance = 8192.0
+	print(dof_distance, " ", far_distance)
+	far_distance_smoothed = lerp(far_distance_smoothed, far_distance, delta * DOF_speed)
+	near_distance_smoothed = lerp(near_distance_smoothed, near_distance, delta * DOF_speed)
+	world_enviroment.camera_attributes.dof_blur_far_distance = far_distance_smoothed
+	world_enviroment.camera_attributes.dof_blur_far_transition = far_distance_smoothed * 2
+	world_enviroment.camera_attributes.dof_blur_near_distance = near_distance_smoothed / 2
+	world_enviroment.camera_attributes.dof_blur_near_transition = near_distance_smoothed / 2
